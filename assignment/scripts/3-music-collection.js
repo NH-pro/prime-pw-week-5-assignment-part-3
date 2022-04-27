d4dd83217f625d62d51372f9d13f2f6c2700cce3
Neil Hanson <pro.neilhanson@gmail.com>
2022-04-27 09:05:14 -0500

    finished 'showCollection' function and tested

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,6 +1,6 @@
 console.log('***** Music Collection *****')
 let collection = [];
-// End var 'collection'.
+// END var 'collection'.
 
 function addToCollection(title, artist, yearPublished) {
     let album = {
@@ -17,7 +17,7 @@ function addToCollection(title, artist, yearPublished) {
     return album;
     // returns the album object to the console.
 }
-// End function 'addToCollection'.
+// END of 'addToCollection' function.
 
 console.log(addToCollection('Americana', 'The Offspring', 1998));
 console.log(addToCollection('Showbiz', 'Muse', 1999));
@@ -25,8 +25,21 @@ console.log(addToCollection('Smash', 'The Offspring', 1994));
 console.log(addToCollection('Where the Light Is', 'Surfaces', 2019));
 console.log(addToCollection('Non-Stop Erotic Cabaret', 'Soft Cell', 1981));
 console.log(addToCollection('Origin of Symmetry', 'Muse', 2001));
-// Testing if 'addToCollection' function works by executing it 6 times with different arguments.
+// TEST if 'addToCollection' function works by executing it 6 times with different arguments.
 
 console.log("Here is your music collection:",collection);
-// Testing to see if 'collection' logs out correctly.
+// TEST to see if 'collection' logs out correctly.
 
+function showCollection(array) {
+    console.log(array.length);
+    // Console logs the number of items in a certain array.
+
+    for (item of array){
+        console.log(`${item.title} by ${item.artist}, published in ${item.yearPublished}.`)
+    }
+    // Console logs a loop of each item in certain array. Foramtted to show title by artist and published year.
+}
+// END of 'showCollection' function.
+
+console.log('Above should be my music collection ^',showCollection(collection));
+// TEST to see if 'showCollection' properly works.
