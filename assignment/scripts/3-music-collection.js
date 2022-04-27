54f14d64171ead42cc3e3d9861f0baf0f07c5b54
Neil Hanson <pro.neilhanson@gmail.com>
2022-04-26 21:04:36 -0500

    got the basics done for the addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,3 +2,24 @@ console.log('***** Music Collection *****')
 let collection = [];
 // End var 'collection'.
 
+function addToCollection(title, artist, yearPublished) {
+    let album = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
+    };
+    // creates 'album' object.
+
+    collection.push(album);
+    // pushes 'album' object into 'collection' array.
+    
+    console.log(`You've added the album ${title} to your collection!`);
+    return album;
+    // returns the album object to the console.
+}
+// End function 'addToCollection'.
+
+console.log(addToCollection('Americana', 'The Offspring', 1998));
+// Testing 
+console.log(addToCollection('Showbiz', 'Muse', 1999));
+
